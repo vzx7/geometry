19cd44171b01b5c49518fb6471d9c25314643b3a
vz <coder.vz@gmail.com>
2022-01-29 20:07:13 +0900

    доработано решение для Гиперболы

--- src/Hyperbola.cpp
+++ src/Hyperbola.cpp
@@ -4,11 +4,14 @@
 
 #include "headers/Function.h"
 
-class Hyperbola : public Function
-{
+class Hyperbola : public Function {
 public:
-    string evaluate(double x)
-    {
-        return "";
+    string evaluate(double x) {
+        double y;
+        if (x < -a || x > a) {
+            y = sqrt((b * b * x * x) / (a * a) - (b * b));
+        } else throw "для элипса x должен находиться в диапазоне a < x > -a!";
+
+        return "(y1=" + to_string(y) + ", y2=" + to_string(-y) + ")";
     }
 };
